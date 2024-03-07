5b3e812274fccb9fa7d5d5d21f19d083d18fb80b
Kh.mamoune <khmamoune33@gmail.com>
2024-03-07 11:27:32 +0100

    added basic login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,15 @@
 function Login() {
   return (
-    <p>Login</p>
+    <>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+        <label htmlFor="pass">Password:</label>
+        <input type="password" id="pass" />
+        <button type="submit"></button>
+      </form>
+    </>
   )
 }
 
