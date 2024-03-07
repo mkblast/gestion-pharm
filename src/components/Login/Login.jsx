8b76413b8bc0efb198212ab61bb9e4baa7d31452
Karim Mk <mk47blast@protonmail.com>
2024-03-07 15:10:05 +0100

    Refactoring the login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,26 @@
 import Form from "../Form/Form";
-import Styles from "../Login/Login.module.css"
+import Styles from "./Login.module.css"
+import Image from "../../assets/Login.jpg"
+import Header from "../Header/Header";
+
 function Login() {
   return (
     <>
-    <div className = {Styles.loginContainer}>
-      <div className={Styles.formContainer}>
-      <h1>Login</h1>
-        <form className={Styles.form}>
-          <Form label="Username " name="name" type="text" />
-          <Form label="Password " name="pass" type="password" />
-          <div className={Styles.buttons}>
-              <button type="submit">Log in</button>
-              <button type="reset">Reset</button>
-          </div>
-          
-        </form>
-
+      <Header />
+      <div className={Styles.container}>
+        <img src={Image} alt="Login Image" className={Styles.image} />
+        <div className={Styles.loginContainer}>
+          <h1>Login</h1>
+          <form className={Styles.form}>
+            <Form label="Username " name="name" type="text" />
+            <Form label="Password " name="pass" type="password" />
+            <div className={Styles.buttons}>
+              <button type="submit" className={Styles.button}>Log in</button>
+              <button type="reset" className={Styles.button}>Reset</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-    
-      
     </>
   )
 }
